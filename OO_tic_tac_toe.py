c4050bd84a10bc12ccdea42f17555e5fcb8d0cd5
Hanny <falsemoo@gmail.com>
2020-01-16 19:35:53 -0700

    lots of comments

--- OO_tic_tac_toe.py
+++ OO_tic_tac_toe.py
@@ -1,32 +1,53 @@
+'''
+Creates a game of tic tac toe
+'''
+#define the main function of the program
 def main() :
+    #create a board object
     board = Board()
+    #set the first turn
     turn = 'X';
     playing = True;
-    
+    #repeats rounds of the game until no longer playing
     while(playing):
+        #print the board
         board.print_me()
+        #say who is playing and get their choice of move
         print("It's your turn " + turn)
+        #get input from the player. Comes as text, so convert to number
         x = int(input("x position? "))
         y = int(input("y position? "))
+        #change the board with a move
         board.place_checker(turn, x, y)
+        #swap whose turn it is
         if turn == 'X':
             turn = 'Y'
         else:
             turn = 'X'
-     
+
+#call the main function     
 main()
 
+#define the board class
 class Board:
+    #initialize a board object. 'self' refers to the current object
     def __init__(self):
+        #define an empty board layout 
         self._layout = [['_', '_', '_'],
                         ['_', '_', '_'], 
                         ['_', '_', '_']]
 
+    #prints every board character in a nice format
     def print_me(self):
+         #loop over every outer item in 2d list
          for i in range(0, len(self._layout)):
+             #loop over inner items
             for j in range(0, len(self._layout[i])):
+                #print on same line by setting end to blank
                 print(board[i][j] + " ", end='')
+            #print on the next line
             print()
 
-    def place_checker(self, turn, x, y):
-        self._layout[y][x] = turn;
+    #change the board layout with a checker
+    def place_checker(self, checker, x, y):
+        self._layout[y][x] = checker;
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,26 +1,52 @@
+'''
+Creates a game of tic tac toe
+'''
+
+#define the main function
 def main() :
+    #make a list of empty board spots using _ character 
+    #and assign it to a variable named board
     board = [['_', '_', '_'],
              ['_', '_', '_'], 
              ['_', '_', '_']]
    
+    #set the first player as X
     turn = 'X';
+    #make a variable to decide if we're still playing
     playing = True;
     
+    #loop ove a round of the game until we aren't playing anymore
     while(playing):
+        #call a function to display the board
         print_board(board)
+        #tell the user whose turn it is
         print("It's your turn " + turn)
+        #ask the user for x and y positions and grab their response.
+        #the response comes back as a string of characters, 
+        #so convert it to an integer
         x = int(input("x position? "))
         y = int(input("y position? "))
+        #change the board at position (x,y) to be the checker
+        #of this player
         board[y][x] = turn;
+        #swap which player's turn it is
         if turn == 'X':
             turn = 'Y'
         else:
             turn = 'X'
-     
+
+#defines a function to print the board in a pretty format
 def print_board(board):
+    #loop from zero to the length of the board in 1 dimension
+    #(outer elements)
     for i in range(0, len(board)):
+        #loop over the other dimension (inner elements)
         for j in range(0, len(board[i])):
+            #display the character in one postion 
+            #set the ending character as blank to
+            #print on one line
             print(board[i][j] + " ", end='')
+        #print on the next line
         print()
 
 main()
